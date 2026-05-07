7679524ffc6d0db2c12dbc237f894c023d479cc5
Danila <204142509+yadanya1337@users.noreply.github.com>
2026-05-07 11:26:52 +0900

    Stop tracking docs folder (move personal notes private)

--- day2_first_request.py
+++ day2_first_request.py
@@ -1,12 +1,10 @@
 import requests
+import json
 
 url = "https://api.github.com/users/torvalds"
 response = requests.get(url)
 data = response.json()
 
-print("Name:", data["name"])
-print("Public repos:", data["public_repos"])
-print("Followers:", data["followers"])
-print("Location:", data["location"])
-print("Bio:", data["bio"])
-print("Created at:", data["created_at"])
+print(json.dumps(data, indent=2))
+
+
